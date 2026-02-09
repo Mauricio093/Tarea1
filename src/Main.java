986735526c5b7f0480a2dd15c8da51538d27aac2
CristelQ28 <cristelquesada14@gmail.com>
2026-02-08 20:52:17 -0600

    Create Main.java
    Implementado por Cristel

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese su nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese su apellido: ");
+        String apellido = sc.nextLine();
+
+        System.out.print("Ingrese su email: ");
+        String email = sc.nextLine();
+
+        System.out.print("Ingrese su telefono: ");
+        String telefono = sc.nextLine();
+
+        System.out.print("Ingrese su direccion: ");
+        String direccion = sc.nextLine();
+
+        System.out.println(" --RESUMEN DE DATOS-- ");
+        System.out.println("Nombre completo: " + nombre + " " + apellido);
+        System.out.println("Email: " + email);
+        System.out.println("Telefono: " + telefono);
+        System.out.println("Direccion: " + direccion);
+
+        sc.close();
+
+    }
+
+}
