fdbb29c358116417f6d963947365fd1fa74c3bde
Gabriel <gabsc28@gmail.com>
2026-02-10 22:15:45 -0600

    Cambio de Gabriel

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         System.out.print("Ingrese su telefono: ");
         String telefono = sc.nextLine();
 
-        System.out.print("Ingrese su direccion: ");
+        System.out.print("Ingrese su dirección: ");
         String direccion = sc.nextLine();
 
         System.out.println(" --RESUMEN DE DATOS-- ");
